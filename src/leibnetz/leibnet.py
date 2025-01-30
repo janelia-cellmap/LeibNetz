debd927f0db6985c394f3d5eadf5a00bfd02a9b8
Jeff Rhoades <37990507+rhoadesScholar@users.noreply.github.com>
2025-01-30 16:15:18 -0500

    Add safe fallback for MPS ops

--- src/leibnetz/leibnet.py
+++ src/leibnetz/leibnet.py
@@ -5,6 +5,9 @@ import torch
 from torch.nn import Module
 import numpy as np
 from leibnetz.nodes import Node
+import os
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 # from funlib.learn.torch.models.conv4d import Conv4d
 
