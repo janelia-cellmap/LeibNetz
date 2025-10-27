991ff953f388fe504774b4ca32f5f4014c4afcf0
rhoadesScholar <rhoadesj@hhmi.org>
2025-10-27 14:54:40 -0400

    Checkpoint from VS Code for coding agent session

--- src/leibnetz/__init__.py
+++ src/leibnetz/__init__.py
@@ -1,3 +1,11 @@
 from .leibnet import LeibNet
 from .nets import build_unet, build_scale_net, build_attentive_scale_net
 from .model_wrapper import ModelWrapper
+from .local_learning import (
+    convert_to_bio,
+    convert_to_backprop,
+    HebbsRule,
+    KrotovsRule,
+    OjasRule,
+    GeometricConsistencyRule,
+)
--- src/leibnetz/nets/__init__.py
+++ src/leibnetz/nets/__init__.py
@@ -1,13 +1,5 @@
 from .attentive_scalenet import build_attentive_scale_net
 from .scalenet import build_scale_net
 from .unet import build_unet
-from .local_learning import (
-    convert_to_bio,
-    convert_to_backprop,
-    HebbsRule,
-    KrotovsRule,
-    OjasRule,
-    GeometricConsistencyRule,
-)
 
 # from .resnet import build_resnet
