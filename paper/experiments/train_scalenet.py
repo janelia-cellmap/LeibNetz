d7fe101d87e4ae8d3b30e41fdf8d18a721041dd0
rhoadesScholar <rhoadesj@hhmi.org>
2025-10-31 13:40:32 -0400

    Add mini-batch size and gradient accumulation steps for scalenet training optimization

--- paper/experiments/train_scalenet.py
+++ paper/experiments/train_scalenet.py
@@ -56,6 +56,10 @@ target_array_info = model.output_shapes
 # Define the optimizer
 optimizer = optimizer(model.parameters())  # optimizer to use for training
 
+mini_batch_size = 16
+gradient_accumulation_steps = batch_size // mini_batch_size
+batch_size = mini_batch_size
+
 if __name__ == "__main__":
     from cellmap_segmentation_challenge import train
 
